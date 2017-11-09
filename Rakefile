bd03e376a656350786481bd6e20a876a5b1ddcd3
Artem <q3pp@yandex.ru>
2017-11-09 23:52:11 +0100

    create rakefle

--- Rakefile
+++ Rakefile
@@ -1,1 +1,6 @@
 require_relative './start_bot'
+
+desc 'run the bot'
+task :run do
+  RandomStupidNameBot.run
+end
--- start_bot.rb
+++ start_bot.rb
@@ -10,17 +10,22 @@ include Helpers
 include MessageAnswer
 include InlineAnswer
 
-token = '143189706:AAEXwSs96fpAuifImBg8NwsIdJoX56eD76k'
+module RandomStupidNameBot
 
-Telegram::Bot::Client.run(token, logger: Logger.new($stdout)) do |bot|
-  @dictionary = Dictionary.new.dictionary
-  bot.listen do |message|
-    puts message.inspect
-    case message
-    when Telegram::Bot::Types::InlineQuery
-      answer_to_inline(message, bot)
-    when Telegram::Bot::Types::Message
-      answer_to_message(message, bot)
+  def self.run
+    token = '143189706:AAEXwSs96fpAuifImBg8NwsIdJoX56eD76k'
+
+    Telegram::Bot::Client.run(token, logger: Logger.new($stdout)) do |bot|
+      @dictionary = Dictionary.new.dictionary
+      bot.listen do |message|
+        puts message.inspect
+        case message
+        when Telegram::Bot::Types::InlineQuery
+          answer_to_inline(message, bot)
+        when Telegram::Bot::Types::Message
+          answer_to_message(message, bot)
+        end
+      end
     end
   end
 end
